a4367a25d0b7da8fbfd1867837340c5570c60a8a
LiYuanzhi <liyuanzhi@LiYuanzhideMBP.attlocal.net>
2022-08-21 22:10:42 -0700

    edit seed

--- seed.py
+++ seed.py
@@ -1,23 +1,42 @@
-DROP DATABASE IF EXISTS capstone1;
+from csv import DictReader
+from app import db
+from models import User, Post
 
-CREATE DATABASE capstone1;
 
-\c capstone1
+db.drop_all()
+db.create_all()
 
-CREATE TABLE users(
-    id serial PRIMARY KEY,
-    username VARCHAR (50) UNIQUE NOT NULL,
-    password VARCHAR (50) NOT NULL,
-    email VARCHAR (50) UNIQUE NOT NULL,
-    create_on TIMESTAMP NOT NULL,
-    FOREIGN KEY (post_id)
-        REFERENCES POSTS (id),
-);
 
-CREATE TABLE POSTS(
-    id serial PRIMARY KEY,
-    post TEXT NOT NULL,
-    FOREIGN KEY (post_id)
-        REFERENCES POSTS (id),
-);
+with open('generator/users.csv') as users:
+    db.session.bulk_insert_mappings(User, DictReader(users))
+
+with open('generator/posts.csv') as messages:
+    db.session.bulk_insert_mappings(Post, DictReader(posts))
+
+
+db.session.commit()
+
+
+# DROP DATABASE IF EXISTS capstone1;
+
+# CREATE DATABASE capstone1;
+
+# \c capstone1
+
+# CREATE TABLE users(
+#     id serial PRIMARY KEY,
+#     username VARCHAR (50) UNIQUE NOT NULL,
+#     password VARCHAR (50) NOT NULL,
+#     email VARCHAR (50) UNIQUE NOT NULL,
+#     create_on TIMESTAMP NOT NULL,
+#     FOREIGN KEY (post_id)
+#         REFERENCES POSTS (id),
+# );
+
+# CREATE TABLE POSTS(
+#     id serial PRIMARY KEY,
+#     post TEXT NOT NULL,
+#     FOREIGN KEY (post_id)
+#         REFERENCES POSTS (id),
+# );
 
