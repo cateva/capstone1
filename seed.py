03ddf73c079bf86027d410817afade17163b2cfb
LiYuanzhi <liyuanzhi@LiYuanzhideMBP.attlocal.net>
2022-08-22 19:05:32 -0700

    edit seed.py

--- seed.py
+++ seed.py
@@ -1,10 +1,10 @@
 # from csv import DictReader
-# from app import db
+from app import db
 # from models import User, Post
 
 
-# db.drop_all()
-# db.create_all()
+db.drop_all()
+db.create_all()
 
 
 # with open('generator/users.csv') as users:
@@ -14,29 +14,6 @@
 #     db.session.bulk_insert_mappings(Post, DictReader(posts))
 
 
-# db.session.commit()
+db.session.commit()
 
-
-DROP DATABASE IF EXISTS capstone1;
-
-CREATE DATABASE capstone1;
-
-\c capstone1
-
-CREATE TABLE users(
-    id serial PRIMARY KEY,
-    username VARCHAR (50) UNIQUE NOT NULL,
-    password VARCHAR (50) NOT NULL,
-    email VARCHAR (50) UNIQUE NOT NULL,
-    create_on TIMESTAMP NOT NULL,
-    FOREIGN KEY (post_id)
-        REFERENCES POSTS (id),
-);
-
-CREATE TABLE POSTS(
-    id serial PRIMARY KEY,
-    post TEXT NOT NULL,
-    FOREIGN KEY (post_id)
-        REFERENCES POSTS (id),
-);
 
